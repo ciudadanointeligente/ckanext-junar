3cf045baaee3fa9c62a6cadf2350ac71d8c1ba67
Felipe Álvarez <falvarez@votainteligente.cl>
2012-01-20 13:11:10 -0300

    added structure and first test

---
+++ ckanext/junar/__init__.py
@@ -0,0 +1,28 @@
+# this is a namespace package
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
+
+
+
+import logging
+log = logging.getLogger(__name__)
+
+import html
+from genshi.core import TEXT
+from genshi.input import HTML
+from genshi.filters import Transformer
+
+from ckan.plugins.core import SingletonPlugin, implements
+from ckan.plugins.interfaces import IConfigurable, IGenshiStreamFilter
+
+
+class Junar(SingletonPlugin):
+    
+    implements(IGenshiStreamFilter)
+    
+    def configure(self, config):
+        pass
---
+++ ckanext/junar/tests/tests.py
@@ -0,0 +1,70 @@
+from ckanext.junar import Junar
+import unittest
+from ckan import plugins
+from sqlalchemy import MetaData, __version__ as sqav
+from nose.tools import assert_equal, raises
+
+from ckan.tests import *
+import ckan.model as model
+from ckan.lib.create_test_data import CreateTestData
+from mock import Mock
+
+
+class TestJunar(unittest.TestCase):
+    
+    @classmethod
+    def setup_class(cls):
+        plugins.load('junar')
+
+    def setUp(self):
+        self.pkgname = u'resourcetest'
+        assert not model.Package.by_name(self.pkgname)
+        assert model.Session.query(model.Resource).count() == 0
+        self.urls = [u'http://somewhere.com/', u'http://elsewhere.com/']
+        self.format = u'csv'
+        self.description = u'Important part.'
+        self.hash = u'abc123'
+        self.alt_url = u'http://alturl' 
+        self.size = 200
+        self.label = 'labeltest'
+        self.sort_order = '1'
+        rev = model.repo.new_revision()
+        pkg = model.Package(name=self.pkgname)
+        model.Session.add(pkg)
+        rg = pkg.resource_groups[0]
+        for url in self.urls:
+            pr = model.Resource(url=url,
+                                format=self.format,
+                                description=self.description,
+                                hash=self.hash,
+                                alt_url=self.alt_url,
+                                extras={u'size':self.size},
+                                )
+            rg.resources.append(pr)
+        pr = model.Resource(url="no_extra",
+                            format=self.format,
+                            description=self.description,
+                            hash=self.hash,
+                            )
+        rg.resources.append(pr)
+        model.repo.commit_and_remove()
+
+    def teardown(self):
+        model.repo.rebuild_db()
+
+    @classmethod
+    def teardown_class(cls):
+        plugins.reset()
+    
+    
+    def test_when_saving_a_resource_it_gets_a_new_guid_from_junar(self):
+       pkg = model.Package.by_name(self.pkgname)
+       rev = model.repo.new_revision()
+       newurl = u'http://xxxxxxxxxxxxxxx'
+       resource = model.Resource(url=newurl)
+       rg = pkg.resource_groups[0]
+
+       rg.resources.insert(0, resource)
+       model.repo.commit_and_remove()
+
+       rg = model.Package.by_name(self.pkgname).resource_groups[0]
