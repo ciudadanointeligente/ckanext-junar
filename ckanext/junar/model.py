15fcccd4674eec094e701539197328c768484d45
Felipe Álvarez <falvarez@votainteligente.cl>
2012-03-28 17:31:07 -0300

    fixing new issues that were not allowing us to publish to junar

--- ckanext/junar/model.py
+++ ckanext/junar/model.py
@@ -21,15 +21,7 @@ def setup():
         create_apps_tables()
     metadata.create_all()
 
-class ResourceRelatedElement(DomainObject):
-    @classmethod
-    def generate_name(cls, title):
-        return _generate_name(cls,title)
-    
-def create_apps_tables():
-    global resource_related_element_table
-    
-    resource_related_element_table = Table('resource_related_element',metadata,
+resource_related_element_table = Table('resource_related_element',metadata,
         Column('id', types.UnicodeText, primary_key=True, default=make_uuid),
         Column('resource_id', types.UnicodeText,
             ForeignKey('resource.id')),
@@ -40,18 +32,29 @@ def create_apps_tables():
         Column('created', DateTime, default=datetime.datetime.now),
         Column('updated', DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now),
     )
-    ## Mappers
 
-    mapper(ResourceRelatedElement, resource_related_element_table, properties={
-        'resource':orm.relation(Resource,
-            # all resources including deleted
-            # formally resource_related_stuff
-            backref=orm.backref('related_elements',
-                                cascade='all, delete',
-                                ),
-                           )
-        },
-    )
+class ResourceRelatedElement(DomainObject):
+    @classmethod
+    def generate_name(cls, title):
+        return _generate_name(cls,title)
+## Mappers
+
+mapper(ResourceRelatedElement, resource_related_element_table, properties={
+    'resource':orm.relation(Resource,
+        # all resources including deleted
+        # formally resource_related_stuff
+        backref=orm.backref('related_elements',
+                            cascade='all, delete',
+                            ),
+                       )
+    },
+)
+
+def create_apps_tables():
+    global resource_related_element_table
+    
+    
+
     
     
 
